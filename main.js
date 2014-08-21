83c9295624bb65f29428ca51505f32143cb1adc0
d3z <d3z@joocy.com>
2014-08-21 22:17:08 +0100

    proxy requests to target

--- main.js
+++ main.js
@@ -2,4 +2,28 @@
 
   'use strict';
 
+  var config = require('./conf');
+  var http = require('http');
+  var httpProxy = require('http-proxy');
+
+  var proxy = httpProxy.createProxyServer({});
+
+  var server = http.createServer(function(request, response){
+    var host = request.headers.host;
+    var target = config.routing_table[host];
+    if (!target) {
+      response.writeHead(400, {'Content-Type':'text/plain'});
+      response.end('Unknown host');
+    }
+    else {
+      if (!isNaN(target)) {
+        target = config.default_host + ':' + target;
+      }
+      proxy.web(request, response, {target: target});
+    }
+  });
+
+  server.listen(80);
+  console.log('router is running on 127.0.0.1:80');
+
 })();
---
+++ sample_conf.js
@@ -0,0 +1,17 @@
+(function(){
+
+  'use strict';
+
+  var config = {
+
+    default_host:'default_when_only_port_is_supplied_in_routing_table',
+
+    routing_table: {
+      'source_request': 'destination_or_port'
+    }
+
+  };
+
+  exports = config;
+
+})();
